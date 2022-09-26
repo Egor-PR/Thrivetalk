daba76e69412f9da3bb63808337a0ab6e3fafeaa
Egor <beltyugov94@mail.com>
2022-09-26 18:56:32 +0300

    commits

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+
+$(function () {
+    $('.header__menu').click(function () {
+      $('.menu,.header__menu').toggleClass('show-menu')
+    })
+  });
+//   $(function () {
+//     $('.link__add').click(function () {
+//       $('.menu__sub-list').toggleClass('sub-menu')
+  
+//     })
+//   });
+  
+  
+  
+  
